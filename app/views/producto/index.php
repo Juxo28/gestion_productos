630fc8266019c1fc05f9efd3c915aeafd185e1fa
Juxo28 <julianpalaciol2804@gmail.com>
2026-09-03 15:40:21 -0500

    Creacion de gestion persona

---
+++ app/views/producto/index.php
@@ -0,0 +1,16 @@
+<h1> Listado Productos</h1>
+
+<table>
+    <tr>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Categoria</th>
+    </tr>
+    <?php foreach ($productos as $producto): ?>
+        <tr>
+            <td><?= $producto['nombre']?></td>
+            <td><?= $producto['precio']?></td>
+            <td><?= $producto['categoria']?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
